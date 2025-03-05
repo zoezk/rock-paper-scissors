c5d5faff020b0d512b8c5af34912809acd544ced
Christian <hacklypselon@gmail.com>
2025-03-05 22:53:28 +0100

    Finalize the UI

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 
 let humanScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll(".btn");
+const resultsDiv = document.querySelector(".result");
+const roundResult = document.createElement("p");
+const gameResult = document.createElement("p");
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Again?";
+resetBtn.classList.add("reset");
+const computerIcon = document.querySelector(".computer-choice");
 
 function playRound(humanChoice, computerChoice) {
+  computerIcon.textContent =
+    computerChoice === "rock" ? "✊" : computerChoice === "paper" ? "✋" : "✌️";
+
   if (humanChoice === computerChoice) {
     return "It's a tie!";
   }
@@ -32,31 +43,41 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice() {
-  return prompt("Enter rock, paper, or scissors").toLowerCase();
-}
+function playGame(e) {
+  const humanSelection = e.target.id;
+  const computerSelection = getComputerChoice();
+  roundResult.textContent = `${playRound(
+    humanSelection,
+    computerSelection
+  )} (${humanScore}:${computerScore})`;
+  resultsDiv.appendChild(roundResult);
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    if (["rock", "paper", "scissors"].includes(humanSelection)) {
-      const computerSelection = getComputerChoice();
-      alert(playRound(humanSelection, computerSelection));
+  if (humanScore + computerScore === 5) {
+    buttons.forEach((elem) => (elem.disabled = true));
+    console.log("disabled");
+    if (humanScore === computerScore) {
+      gameResult.textContent = `It's a tie! (${humanScore}:${computerScore})`;
+      resultsDiv.appendChild(gameResult);
+    } else if (humanScore > computerScore) {
+      gameResult.textContent = `Player wins! (${humanScore}:${computerScore})`;
+      resultsDiv.appendChild(gameResult);
     } else {
-      alert(
-        "Invalid choice, please enter rock, paper, or scissors. Repeating round..."
-      );
-      i--; // Decrement i to repeat the round
+      gameResult.textContent = `Computer wins! (${computerScore}:${humanScore})`;
+      resultsDiv.appendChild(gameResult);
     }
-  }
-
-  if (humanScore === computerScore) {
-    alert(`It's a tie! (${humanScore}:${computerScore})`);
-  } else if (humanScore > computerScore) {
-    alert(`Player wins! (${humanScore}:${computerScore})`);
-  } else {
-    alert(`Computer wins! (${computerScore}:${humanScore})`);
+    resultsDiv.appendChild(resetBtn);
   }
 }
 
-playGame();
+resetBtn.addEventListener("click", () => {
+  buttons.forEach((elem) => (elem.disabled = false));
+  humanScore = 0;
+  computerScore = 0;
+  resultsDiv.removeChild(roundResult);
+  resultsDiv.removeChild(gameResult);
+  resultsDiv.removeChild(resetBtn);
+});
+
+buttons.forEach((elem) => {
+  elem.addEventListener("click", playGame);
+});
